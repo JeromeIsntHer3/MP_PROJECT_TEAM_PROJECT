fbbcc60167b1458e2dba2a74fe271060e70bff98
JeromeIsntHere <jeromeisnthere@gmail.com>
2022-09-10 14:46:50 +0800

    Update and Fixes

--- Assets/Scripts/PopUps/PopUpStorage.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Pop-up", menuName = "Pop-up/Pop-up Storage")]
-public class PopUpStorage : ScriptableObject
-{
-    public List<PopUpSO> popUps;
-}
